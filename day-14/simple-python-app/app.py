251741be48c1383b4119c88b584217650b2ced66
Godwin Iheuwa <32501366+stargodwin@users.noreply.github.com>
2024-05-03 21:23:26 +0100

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!, New Change'
+    return 'Hello, world!, New Change Now'
 
 if __name__ == '__main__':
     app.run()
